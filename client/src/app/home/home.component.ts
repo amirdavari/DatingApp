9ae70cae5b89ca8fb5d437c6691d47475c53bf52
Amir Davari <amir.davari@gmx.de>
2023-10-06 07:42:34 +0200

    End of section 9

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -6,23 +6,11 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   registerMode = false;
-  users: any;
 
-  constructor(private http: HttpClient) {
+  constructor() {
 
-  }
-  ngOnInit(): void {
-    
-  }
-
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Completed!')
-    });
   }
 
   registerToggle() {
